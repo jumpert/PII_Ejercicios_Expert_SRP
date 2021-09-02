fc91738345d409d2eb77c9f7f048c1f504b98da4
jumpert <juan.perezc@correo.ucu.edu.uy>
2021-09-01 22:50:44 -0300

    Justificación del Ejercicio1

--- Ejercicio 1/src/Library/Libro.cs
+++ Ejercicio 1/src/Library/Libro.cs
@@ -23,6 +23,14 @@ namespace SRP
             this.SectorBiblioteca = sector;
             this.EstanteBiblioteca = estante;
         }
+        /*
+        En este caso la clase Libro tiene el metodo AlmacenarLibro que no es razonable que le pertenezca,
+        es una responsabilidad que bien podria cumplir otra clase.
+        Porque aplicando el principio SRP hay más de una razón que puede hacer que el libro cambie su ubicación
+        o la forma en que se Almacenan los libros.
+        Aplicando el principio SRC al crear otra clase haríamos que las responsabilidad se dividieran en una forma
+        más equitativa y no tenga que hacer todo una sola función
+        */
 
     }
 }
